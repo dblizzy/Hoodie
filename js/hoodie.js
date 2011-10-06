c893a4a433c989939d05ad6acbdd6acf3be0e761
Craig Waterman <craigwaterman@gmail.com>
2011-10-06 10:20:45 -0400

    Styles; javascript; initial documentation
    Cleaned out styles and separated by concern; added all necessary
    javascript; initial docs (culled from Bootstrap);

---
+++ js/hoodie.js
@@ -0,0 +1,12 @@
+/**
+ * @depends selectivizr-min.js
+ * @depends modernizr-2.0.6.min.js
+ * @depends imgsizer.js
+ * @depends bootstrap-alerts.js
+ * @depends bootstrap-dropdown.js
+ * @depends bootstrap-modal.js
+ * @depends bootstrap-popover.js
+ * @depends bootstrap-scrollspy.js
+ * @depends bootstrap-tabs.js
+ * @depends bootstrap-twipsy.js
+ */
